a4b8e8d4a3f9e9638a253e6b860c42d54db00abb
Roxana1981 <roxanaameijide81@gmail.com>
2023-12-25 14:35:28 +0000

    Correct allowed host direction

--- allvintage/settings.py
+++ allvintage/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-3r755^v#pg#8mq0*pzh6hz-w*jb8w61rxu*)xigfj(i4hoba6k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['allvintage.herokuapp.com', 'localhost', '8000-roxana1981-allvintage-lvubrwphex4.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['allvintage-48223a19dde6.herokuapp.com', 'localhost', '8000-roxana1981-allvintage-lvubrwphex4.ws-eu107.gitpod.io']
 
 
 # Application definition
