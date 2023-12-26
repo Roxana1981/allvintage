49990a34259bd17594cf92204b7787f2694c0c42
Roxana1981 <roxanaameijide81@gmail.com>
2023-12-26 18:01:01 +0000

    Update content request templates and update model file

--- content/models.py
+++ content/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 class Content(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Content'
+        
     email = models.EmailField()
     content_type = models.CharField(max_length=255)
     message = models.TextField()
