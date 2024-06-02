b289c4c16ef96a8536319294e49178c11ac991f9
Roxana1981 <roxanaameijide81@gmail.com>
2024-06-02 11:09:58 +0000

    Update URLS for comment edit and deletion

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('delete_comment/<int:pk>', views.DeleteComment.as_view(),
+         name='delete_comment'),
+    path('update_comment/<int:pk>', views.UpdateComment.as_view(),
+         name='update_comment')
 ]
